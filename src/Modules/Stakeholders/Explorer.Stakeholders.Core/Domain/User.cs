e5ed8b84f387c892f3b2f255cf1f2c33632cf303
Filip Filko <filipfilko15@gmail.com>
2025-11-15 19:05:47 +0100

    Add User entity with Email

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/User.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/User.cs
@@ -6,13 +6,15 @@ public class User : Entity
 {
     public string Username { get; private set; }
     public string Password { get; private set; }
+    public string Email { get; private set; }   // Svaki nalog obuhvata i Email
     public UserRole Role { get; private set; }
     public bool IsActive { get; set; }
 
-    public User(string username, string password, UserRole role, bool isActive)
+    public User(string username, string password, string email, UserRole role, bool isActive)
     {
         Username = username;
         Password = password;
+        Email = email;
         Role = role;
         IsActive = isActive;
         Validate();
@@ -20,8 +22,9 @@ public class User : Entity
 
     private void Validate()
     {
-        if (string.IsNullOrWhiteSpace(Username)) throw new ArgumentException("Invalid Name");
-        if (string.IsNullOrWhiteSpace(Password)) throw new ArgumentException("Invalid Surname");
+        if (string.IsNullOrWhiteSpace(Username)) throw new ArgumentException("Invalid Username");
+        if (string.IsNullOrWhiteSpace(Password)) throw new ArgumentException("Invalid Password");
+        if (string.IsNullOrWhiteSpace(Email)) throw new ArgumentException("Invalid Email");
     }
 
     public string GetPrimaryRoleName()
