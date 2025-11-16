bfdd781cdb5cd5aa5c41dc38cd5c022e08c81639
ikabic <kabic.ivona@gmail.com>
2025-11-17 00:14:02 +0100

    feat(tours): extend ToursProfile with Tour mapping - Added enum-string converters for TourDifficulty and TourStatus

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -9,5 +9,13 @@ public class ToursProfile : Profile
     public ToursProfile()
     {
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
+
+        CreateMap<TourDifficulty, string>().ConvertUsing(src => src.ToString());
+        CreateMap<string, TourDifficulty>().ConvertUsing(src => Enum.Parse<TourDifficulty>(src, true));
+
+        CreateMap<TourStatus, string>().ConvertUsing(src => src.ToString());
+        CreateMap<string, TourStatus>().ConvertUsing(static src => Enum.Parse<TourStatus>(src, true));
+
+        CreateMap<Tour, TourDto>().ReverseMap();
     }
 }
