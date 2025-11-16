f82a58efe319369a26d77e5096866fb19a266d13
ikabic <kabic.ivona@gmail.com>
2025-11-17 00:17:09 +0100

    feat(tours): add tour service and interface implementation

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourService.cs
@@ -0,0 +1,12 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+
+namespace Explorer.Tours.API.Public.Administration;
+
+public interface ITourService
+{
+    PagedResult<TourDto> GetPaged(int page, int pageSize);
+    TourDto Create(TourDto Tour);
+    TourDto Update(TourDto Tour);
+    void Delete(long id);
+}
