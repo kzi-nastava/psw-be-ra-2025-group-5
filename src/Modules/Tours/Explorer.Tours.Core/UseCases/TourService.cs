bed74341f104b77461d32b15eb8e192d4db78d2f
ikabic <kabic.ivona@gmail.com>
2025-11-17 00:31:01 +0100

    chore(tours): move misplaced files to correct folder structure

---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/TourService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+
+namespace Explorer.Tours.Core.UseCases;
+
+public class TourService : ITourService
+{
+    private readonly ITourRepository _TourRepository;
+    private readonly IMapper _mapper;
+
+    public TourService(ITourRepository repository, IMapper mapper)
+    {
+        _TourRepository = repository;
+        _mapper = mapper;
+    }
+
+    public PagedResult<TourDto> GetPaged(int page, int pageSize)
+    {
+        var result = _TourRepository.GetPaged(page, pageSize);
+
+        var items = result.Results.Select(_mapper.Map<TourDto>).ToList();
+        return new PagedResult<TourDto>(items, result.TotalCount);
+    }
+
+    public TourDto Create(TourDto entity)
+    {
+        var result = _TourRepository.Create(_mapper.Map<Tour>(entity));
+        return _mapper.Map<TourDto>(result);
+    }
+
+    public TourDto Update(TourDto entity)
+    {
+        var result = _TourRepository.Update(_mapper.Map<Tour>(entity));
+        return _mapper.Map<TourDto>(result);
+    }
+
+    public void Delete(long id)
+    {
+        _TourRepository.Delete(id);
+    }
+}
