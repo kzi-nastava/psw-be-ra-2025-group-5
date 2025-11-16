3974ffe2ac21cf1c8fc3dfa85dcede9af3c6a935
Nikola Vulović <nikolavulovic4@gmail.com>
2025-11-16 16:04:16 +0100

    added Monument and MonumentDbRepository

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Monument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class Monument: Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateOnly Year { get; set; }
+        public MonumentStatus Status { get; set; }
+        public MonumentLocation Location { get; set; }
+
+        public Monument(string Name, string? Description, DateOnly Year, MonumentStatus status, MonumentLocation location)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Year = Year;
+            this.Status = status;
+            this.Location = location;
+        }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/MonumentLocation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class MonumentLocation
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public MonumentLocation(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
