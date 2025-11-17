54aab693467c2416b2bc5a7494631803835bd51a
Svetlana Kis <svetlanakis2003@gmail.com>
2025-11-17 23:20:38 +0100

    feat: add IBlogRepository and BlogRepository with basic CRUD

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/RepositoryInterfaces/IBlogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Blog.Core.Domain.RepositoryInterfaces
+{
+    public interface IBlogRepository
+    {
+        BlogPost? GetById(long id);
+        List<BlogPost> GetByAuthor(long authorId);
+        BlogPost Create(BlogPost blog);
+        BlogPost Update(BlogPost blog);
+    }
+}
---
+++ src/Modules/Blog/Explorer.Blog.Infrastructure/Database/Repositories/BlogDbRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Explorer.Blog.Core.Domain;
+using Explorer.Blog.Core.Domain.RepositoryInterfaces;
+
+namespace Explorer.Blog.Infrastructure.Database.Repositories
+{
+    public class BlogDbRepository: IBlogRepository
+    {
+        private readonly BlogContext _dbContext;
+
+        public BlogDbRepository(BlogContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BlogPost? GetById(long id) => _dbContext.BlogPosts.FirstOrDefault(blog => blog.Id == id);
+        public List<BlogPost> GetByAuthor(long authorId) => _dbContext.BlogPosts.Where(author => author.AuthorId == authorId).ToList();
+        public BlogPost Create(BlogPost blog)
+        {
+            _dbContext.BlogPosts.Add(blog);
+            _dbContext.SaveChanges();
+            return blog;
+        }
+        public BlogPost Update(BlogPost blog)
+        {
+            _dbContext.BlogPosts.Update(blog);
+            _dbContext.SaveChanges();
+            return blog;
+        }
+    }
+}
