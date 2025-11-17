bc35fa9c7e792e543975557250472755dddcf981
Svetlana Kis <svetlanakis2003@gmail.com>
2025-11-17 22:49:21 +0100

    feat: add BlogPost and BlogImage entities (bytea storage)

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/BlogImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class BlogImage: Entity 
+    {
+        public long BlogPostId { get; set; }
+        public byte[] Data { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public int Order { get; set; }
+
+        public BlogImage(long blogPostId, byte[] data, string contentType, string fileName, int order)
+        {
+            BlogPostId = blogPostId;
+            Data = data;
+            ContentType = contentType;
+            FileName = fileName;
+            Order = order;
+            Validate(); 
+        }
+
+        private void Validate()
+        {
+            if (BlogPostId == 0) throw new ArgumentException("Invalid BlogPostId");
+            if (Data is null) throw new ArgumentNullException("Invalid data");
+            if (string.IsNullOrEmpty(FileName)) throw new ArgumentException("Invalid filename");
+            if (!string.IsNullOrEmpty(ContentType)) throw new ArgumentException("Invalid content type");
+        }
+    }
+}
