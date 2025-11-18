5d325bc4ace027685d440d205d747b7a64e1e045
ikabic <kabic.ivona@gmail.com>
2025-11-19 00:40:58 +0100

    chore(tours): add SQL seed script for initial Tour data

---
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/Scripts/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO stakeholders."Users" VALUES (2, 'zika', 'zika', 1, true);
+INSERT INTO stakeholders."Users" VALUES (3, 'mika', 'mika', 1, true);
+
+INSERT INTO tours."Tours" VALUES (1, 'Uvac Canyon Lookout Tour', 'A guided visit to the iconic Uvac meanders, including hiking to the best panoramic viewpoints and observing the griffon vultures.', 1, '{Nature,Scenic,Wildlife}', 0, 0, 2);
+INSERT INTO tours."Tours" VALUES (2, 'Niš WWII History Trail', 'Explore key WWII historical locations in Niš, including the Red Cross Concentration Camp and Bubanj Memorial Park.', 0, '{History,Culture,Education}', 5.05, 1, 2);
+INSERT INTO tours."Tours" VALUES (3, 'Kopaonik Ski & Snow Walk', 'A winter sports experience on Kopaonik with beginner–friendly skiing and guided snow trail walks.', 2, '{}', 0, 0, 3);
