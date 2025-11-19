8c1470833a5c54a796f27f32e2bdfa1a36877662
subotic.l <lukasubotic34@gmail.com>
2025-11-19 21:01:58 +0100

    feat: Add TourProblemController and TourProblemDbRepository

---
+++ src/Explorer.API/Controllers/Tourist/ProblemReporting/TourProblemController.cs
@@ -0,0 +1,47 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Explorer.Tours.Core.UseCases.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist.ProblemReporting
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/problemreporting")]
+    [ApiController]
+    public class TourProblemController : ControllerBase
+    {
+        private readonly ITourProblemService _tourProblemService;
+
+        public TourProblemController(ITourProblemService tourProblemService)
+        {
+            _tourProblemService = tourProblemService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<TourProblemDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            return Ok(_tourProblemService.GetPaged(page, pageSize));
+        }
+
+        [HttpPost]
+        public ActionResult<TourProblemDto> Create([FromBody] TourProblemDto tourProblem)
+        {
+            return Ok(_tourProblemService.Create(tourProblem));
+        }
+
+        [HttpPut("{id:long}")]
+        public ActionResult<TourProblemDto> Update([FromBody] TourProblemDto tourProblem)
+        {
+            return Ok(_tourProblemService.Update(tourProblem));
+        }
+
+        [HttpDelete("{id:long}")]
+        public ActionResult Delete(long id)
+        {
+            _tourProblemService.Delete(id);
+            return Ok();
+        }
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
@@ -25,11 +25,13 @@ public static class ToursStartup
     private static void SetupCore(IServiceCollection services)
     {
         services.AddScoped<IEquipmentService, EquipmentService>();
+        services.AddScoped<ITourProblemService, TourProblemService>();
     }
 
     private static void SetupInfrastructure(IServiceCollection services)
     {
         services.AddScoped<IEquipmentRepository, EquipmentDbRepository>();
+        services.AddScoped<ITourProblemRepository, TourProblemDbRepository>();
 
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("tours"));
         dataSourceBuilder.EnableDynamicJson();
