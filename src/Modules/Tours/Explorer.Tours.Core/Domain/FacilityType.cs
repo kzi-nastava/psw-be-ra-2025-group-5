3d6d2c26c75a65c120264b5906a05b24c4225739
Jovan Bozic <jovanbozic2003@gmail.com>
2025-11-19 16:45:32 +0100

    Add Facility class

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/FacilityType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public enum FacilityType
+    {
+        wc, restaurant, parking, other
+    }
+}
