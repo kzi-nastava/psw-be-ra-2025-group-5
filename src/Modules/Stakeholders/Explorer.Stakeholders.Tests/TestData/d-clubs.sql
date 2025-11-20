77c4be54250f33de912c26d1eec97c29d457b7bf
janaj87 <wordpress.jana@gmail.com>
2025-11-20 06:19:56 +0100

    feat: fix test data

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
@@ -1,4 +1,1 @@
-﻿INSERT INTO stakeholders."Clubs" (Name, Description, Images, CreatorId)
-VALUES ('Planinari', 'Planinarski klub', 0x010203, -11);
-INSERT INTO stakeholders."Clubs" (Name, Description, Images, CreatorId)
-VALUES ('Leto 2026', 'Ponude za letovanje', 0x012203, -21);
+﻿
