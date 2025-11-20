5ad5598a72f555f4be93918fc4610538b5954c93
janaj87 <wordpress.jana@gmail.com>
2025-11-20 19:43:20 +0100

    feat: add seed.sql script

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Scripts/seed.sql
@@ -0,0 +1,44 @@
+﻿INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-1, 'admin@gmail.com', 'admin', 0, true);
+
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-11, 'autor1@gmail.com', 'autor1', 1, true);
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-12, 'autor2@gmail.com', 'autor2', 1, true);
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-13, 'autor3@gmail.com', 'autor3', 1, true);
+
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-21, 'turista1@gmail.com', 'turista1', 2, true);
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-22, 'turista2@gmail.com', 'turista2', 2, true);
+INSERT INTO stakeholders."Users"(
+    "Id", "Username", "Password", "Role", "IsActive")
+VALUES (-23, 'turista3@gmail.com', 'turista3', 2, true);
+
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-11, -11, 'Ana', 'Anić', 'autor1@gmail.com');
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-12, -12, 'Lena', 'Lenić', 'autor2@gmail.com');
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-13, -13, 'Sara', 'Sarić', 'autor3@gmail.com');
+
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-21, -21, 'Pera', 'Perić', 'turista1@gmail.com');
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-22, -22, 'Mika', 'Mikić', 'turista2@gmail.com');
+INSERT INTO stakeholders."People"(
+    "Id", "UserId", "Name", "Surname", "Email")
+VALUES (-23, -23, 'Steva', 'Stević', 'turista3@gmail.com');
+
--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/d-clubs.sql
@@ -1,1 +1,15 @@
-﻿
+﻿INSERT INTO stakeholders.[Clubs] (
+    [Name], [Description], [Images], [CreatorId]
+)
+VALUES (
+    'Planinari',
+    'Planinarski klub',
+    (
+        SELECT CAST(N'' AS XML).value(
+            'xs:base64Binary("iVBORw0KGgoAAAANSUhEUgAAAaQAAAGkCAIAAADxLsZiAAAF+0lEQVR4nOzXQQ3bQBBA0aYKjJAyCYMyCZMygIXQQ6Weqyrdlf3fIzAjjfLjfY8xfgA83c/VCwDMIHZAgtgBCWIHJIgdkCB2QILYAQliBySIHZAgdkCC2AEJYgckiB2QIHZAgtgBCWIHJIgdkCB2QILYAQliBySIHZAgdkCC2AEJYgckiB2QIHZAgtgBCWIHJIgdkDCe9qkaz+nzQJu5HNsE6b4sgMSxA5IEDsgQeyA..." 
+            )',
+            'varbinary(max)'
+        )
+    ),
+    -11
+);
