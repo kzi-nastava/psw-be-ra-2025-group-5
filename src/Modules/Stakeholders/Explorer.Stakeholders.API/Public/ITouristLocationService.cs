12f739f007a033ae319926f564748ba1a38a6815
ikabic <kabic.ivona@gmail.com>
2025-11-25 19:22:27 +0100

    feat(stakeholders): add tourist location service interface and service implementation

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/ITouristLocationService.cs
@@ -0,0 +1,11 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+
+namespace Explorer.Stakeholders.API.Public;
+
+public interface ITouristLocationService
+{
+    TouristLocationDto? Get(long id);
+    IEnumerable<TouristLocationDto> GetAll();
+    TouristLocationDto Create(TouristLocationDto entity);
+    TouristLocationDto Update(TouristLocationDto entity);
+}
