354ccc1117586a7f81687411bfd7917c55c2ac2f
ikabic <kabic.ivona@gmail.com>
2025-11-30 22:36:20 +0100

    feat(tours): add shopping cart service and interface implementation

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Shopping/IShoppingCartService.cs
@@ -0,0 +1,10 @@
+﻿using Explorer.Tours.API.Dtos;
+
+namespace Explorer.Tours.API.Public.Shopping;
+
+public interface IShoppingCartService
+{
+    ShoppingCartDto GetByTourist(long touristId);
+    ShoppingCartDto Create(ShoppingCartDto ShoppingCart);
+    ShoppingCartDto Update(ShoppingCartDto ShoppingCart);
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Shopping/ShoppingCartService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Shopping;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+
+namespace Explorer.Tours.Core.UseCases.Shopping;
+
+public class ShoppingCartService : IShoppingCartService
+{
+    private readonly IShoppingCartRepository _ShoppingCartRepository;
+    private readonly IMapper _mapper;
+
+    public ShoppingCartService(IShoppingCartRepository repository, IMapper mapper)
+    {
+        _ShoppingCartRepository = repository;
+        _mapper = mapper;
+    }
+
+    public ShoppingCartDto GetByTourist(long touristId)
+    {
+        var result = _ShoppingCartRepository.GetByTourist(touristId);
+        return _mapper.Map<ShoppingCartDto>(result);
+    }
+
+    public ShoppingCartDto Create(ShoppingCartDto entity)
+    {
+        var result = _ShoppingCartRepository.Create(_mapper.Map<ShoppingCart>(entity));
+        return _mapper.Map<ShoppingCartDto>(result);
+    }
+
+    public ShoppingCartDto Update(ShoppingCartDto entity)
+    {
+        var result = _ShoppingCartRepository.Update(_mapper.Map<ShoppingCart>(entity));
+        return _mapper.Map<ShoppingCartDto>(result);
+    }
+}
