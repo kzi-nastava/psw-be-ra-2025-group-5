9d1a94b622433b51151165c3e4e1222f9304b93f
Svetlana Kis <svetlanakis2003@gmail.com>
2025-12-01 20:44:11 +0100

    feat: add base models for tour execution functionality

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/KeyPointCompletion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain; 
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class KeyPointCompletion : ValueObject
+    {
+        public long KeyPointId { get; }
+        public DateTime CompletedAt { get; }
+        public double DistanceTravelled { get; }
+
+        public KeyPointCompletion(long keyPointId, DateTime completedAt, double distanceTravelled)
+        {
+            KeyPointId = keyPointId;
+            CompletedAt = completedAt;
+            DistanceTravelled = distanceTravelled;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return KeyPointId;
+            yield return CompletedAt;
+        }
+    }
+}
