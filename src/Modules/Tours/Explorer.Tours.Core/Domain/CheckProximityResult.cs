e555b825850ca283e210a9b03e372cdd6eabe06d
Svetlana Kis <svetlanakis2003@gmail.com>
2025-12-02 19:22:19 +0100

    feat: add repository and its interface for tour execution

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/CheckProximityResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class CheckProximityResult
+    {
+        public bool IsNearKeyPoint { get; }
+        public long? CompletedKeyPointId { get; }
+        public DateTime? CompletedAt { get; }
+        public bool UnlockedSecret { get; }
+        public double PercentCompleted { get; }
+        public DateTime LastActivity { get; }
+        public KeyPoint? NextKeyPoint { get; }
+
+        public CheckProximityResult(
+            bool isNearKeyPoint,
+            long? completedKeyPointId,
+            DateTime? completedAt,
+            bool unlockedSecret,
+            double percentCompleted,
+            DateTime lastActivity,
+            KeyPoint? nextKeyPoint
+        )
+        {
+            IsNearKeyPoint = isNearKeyPoint;
+            CompletedKeyPointId = completedKeyPointId;
+            CompletedAt = completedAt;
+            UnlockedSecret = unlockedSecret;
+            PercentCompleted = percentCompleted;
+            LastActivity = lastActivity;
+            NextKeyPoint = nextKeyPoint;
+        }
+    }
+}
