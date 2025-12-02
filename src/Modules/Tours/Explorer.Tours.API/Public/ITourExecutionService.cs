626e91b1465a21d1886033bb94a86206370ada7a
Svetlana Kis <svetlanakis2003@gmail.com>
2025-12-02 19:22:59 +0100

    feat: add service and its interface for tour execution

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/ITourExecutionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Tours.API.Dtos;
+
+namespace Explorer.Tours.API.Public
+{
+    public interface ITourExecutionService
+    {
+        StartExecutionResultDto StartExecution(long userId, long tourId);
+        CheckProximityDto CheckProximity(long executionId, LocationDto location);
+        void CompleteExecution(long executionId);
+        void AbandonExecution(long executionId);
+        TourExecutionDto GetExecution(long executionId); 
+    }
+}
