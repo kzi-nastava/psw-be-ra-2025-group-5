ece9eaf16a0293941e876b17648e4d50eb758360
Nikolina <hrgic.ra241.2023@uns.ac.rs>
2025-12-17 12:47:34 +0100

    feat:Recommit digital diary feature after branch was removed

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Diary.cs
@@ -0,0 +1,38 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Stakeholders.Core.Domain;
+
+public enum DiaryStatus
+{
+    Draft = 0,
+    Published = 1
+}
+
+public class Diary : Entity
+{
+    public string Name { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DiaryStatus Status { get; private set; }
+    public string Country { get; private set; }
+    public string? City { get; private set; }
+    public long TouristId { get; private set; }
+
+    public Diary(string name, DateTime createdAt, DiaryStatus status, string country, string? city, long touristId)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Diary name cannot be empty.");
+
+        if (string.IsNullOrWhiteSpace(country))
+            throw new ArgumentException("Country cannot be empty.");
+
+        //if (touristId <= 0)
+        //    throw new ArgumentException("Invalid Tourist ID.");
+
+        Name = name;
+        CreatedAt = createdAt;
+        Status = status;
+        Country = country;
+        City = string.IsNullOrWhiteSpace(city) ? null : city;
+        TouristId = touristId;
+    }
+}
