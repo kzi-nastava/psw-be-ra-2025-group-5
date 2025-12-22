131e4a02dd25aba7c8ca024d366357fe9783a312
janaj87 <wordpress.jana@gmail.com>
2025-12-22 15:35:57 +0100

    test: add test file storage for images

---
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Tests/InMemoryImageStorage.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.BuildingBlocks.Core.FileStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.BuildingBlocks.Tests
+{
+    public class InMemoryImageStorage : IImageStorage
+    {
+        public Dictionary<string, byte[]> StoredImages { get; } = new();
+
+        public string SaveImage(string type, long id, byte[] content, string contentType)
+        {
+            var path = $"/fake/{type}/{id}/{Guid.NewGuid()}.jpg";
+            StoredImages[path] = content;
+            return path;
+        }
+
+        public void Delete(string path)
+        {
+            StoredImages.Remove(path);
+        }
+    }
+}
