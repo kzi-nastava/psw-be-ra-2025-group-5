772410735120c80333bc1a210a7385efed6f2e46
Aleksa-Bijelic <aleksabjlc@gmail.com>
2025-12-22 12:28:18 +0100

    feat: add encounters module and add challenge crud

---
+++ src/Modules/Encounters/Explorer.Encounters.Infrastructure/Database/EncountersContext.cs
@@ -0,0 +1,17 @@
+using Explorer.Encounters.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Explorer.Encounters.Infrastructure.Database
+{
+    public class EncountersContext : DbContext
+    {
+        public DbSet<Challenge> Challenges { get; set; }
+
+        public EncountersContext(DbContextOptions<EncountersContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("encounters");
+        }
+    }
+}
