96d3d552f2406b01f9b6abb3c74fafeb50cc6116
Jovan Bozic <jovanbozic2003@gmail.com>
2025-12-25 20:38:04 +0100

    tests: add TourAnalyticsQueryTests

---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/TourAuthoring/TourAnalyticsQueryTests.cs
@@ -0,0 +1,36 @@
+﻿using Explorer.API.Controllers.Author;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Tours.Tests.Integration.TourAuthoring
+{
+    [Collection("Sequential")]
+    public class TourAnalyticsQueryTests : BaseToursIntegrationTest
+    {
+        public TourAnalyticsQueryTests(ToursTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void GetToursCountByPrice_ReturnsData()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = scope.ServiceProvider.GetRequiredService<AuthorAnalyticsController>();
+
+            var userId = -11;
+
+            // Act
+            var result = ((OkObjectResult)controller.GetToursCountByPrice(userId).Result)?.Value
+                         as IReadOnlyCollection<ToursByPriceDto>;
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.Count.ShouldBeGreaterThan(0);
+            result.All(r => !string.IsNullOrEmpty(r.PriceRange)).ShouldBeTrue();
+            result.All(r => r.Count >= 0).ShouldBeTrue();
+        }
+    }
+}
