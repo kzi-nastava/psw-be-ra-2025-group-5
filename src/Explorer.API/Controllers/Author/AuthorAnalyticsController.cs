488bb59d363a05d70aab3c1750fd19c2d98143f8
Jovan Bozic <jovanbozic2003@gmail.com>
2025-12-25 12:50:55 +0100

    feat: Add AuthorAnalyticsController

---
+++ src/Explorer.API/Controllers/Author/AuthorAnalyticsController.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.Tours.API.Internal;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/tours/analytics")]
+    [ApiController]
+    public class AuthorAnalyticsController : ControllerBase
+    {
+        private readonly ITourStatisticsService _tourStatisticsService;
+
+        public AuthorAnalyticsController(ITourStatisticsService tourStatisticsService)
+        {
+            _tourStatisticsService = tourStatisticsService;
+        }
+
+        //[HttpGet("purchased/count/{userId:long}")]
+        //public ActionResult<int> GetPurchasedToursCount(long userId)
+        //{
+        //    return Ok(_tourStatisticsService.GetPurchasedToursCount(userId));
+        //}
+
+        //[HttpGet("completed/{userId:long}")]
+        //public ActionResult<IReadOnlyCollection<TourStatisticsItemDto>> GetCompletedTours(long userId)
+        //{
+        //    return Ok(_tourStatisticsService.GetCompletedTours(userId));
+        //}
+    }
+}
