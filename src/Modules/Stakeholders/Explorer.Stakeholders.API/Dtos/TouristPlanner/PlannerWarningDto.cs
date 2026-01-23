f29e5557029b64d1a705cc658358681dfa63a208
Svetlana Kis <svetlanakis2003@gmail.com>
2026-01-23 18:35:12 +0100

    feat: add planner warning model and dto

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/TouristPlanner/PlannerWarningDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos.TouristPlanner
+{
+    public class PlannerWarningDto
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+        public List<long> AffectedBlockIds { get; set; }
+    }
+}
